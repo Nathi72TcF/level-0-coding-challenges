c757fbbbb4ce0ffd5cf3bca6c6c03d26cc7a0225
Nkosinathi <50706503+Nathi72TcF@users.noreply.github.com>
2021-09-24 21:23:44 +0200

    im real having hard time solving this task, been stuck for 4 days

--- task_8.py
+++ task_8.py
@@ -1,10 +1,13 @@
 def convert_time(time):
     time = time % (24 * 3600)
     hour = time // 60
+
     time %= 3600
     minutes = time // 60
+
     time %= 60
     seconds = time
+    
     if hour == 1:
         print("%02d hour %02d minutes %02d seconds" % (hour, minutes, seconds))
     else:
